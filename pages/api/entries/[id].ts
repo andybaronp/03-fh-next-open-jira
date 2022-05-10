c515eba022de8767b31cbdce6cb3ac611adad3ca
Jhoandris B <andybaronp@gmail.com>
2022-05-09 19:06:21 -0500

    get Entry DB

--- pages/api/entries/[id].ts
+++ pages/api/entries/[id].ts
@@ -22,6 +22,8 @@ export default function handler(
   switch (req.method) {
     case 'PUT':
       return updateEntry(req, res)
+    case 'GET':
+      return getEntry(req, res)
 
     default:
       return res.status(400).json({ message: 'Metodo no existe' })
@@ -57,4 +59,18 @@ const updateEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     console.log(error)
     res.status(400).json({ message: error.errors.status.message })
   }
+}
+
+const getEntry = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { id } = req.query
+
+  await db.connect()
+
+  const entryInDB = await Entry.findById(id)
+  await db.disconnect()
+
+  if (!entryInDB) {
+    return res.status(400).json({ message: 'No hay entrada con ese id' + id })
+  }
+  return res.status(200).json(entryInDB)
 }
