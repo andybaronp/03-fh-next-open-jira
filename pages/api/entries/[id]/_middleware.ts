c819d0be7258c17c9e025fc26fcc3be7d994a394
Jhoandris B <andybaronp@gmail.com>
2022-05-09 20:17:54 -0500

    Middleware id

---
+++ pages/api/entries/[id]/_middleware.ts
@@ -0,0 +1,19 @@
+import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req: NextRequest, ev: NextFetchEvent) {
+  const id = req.page.params?.id || ''
+  const checkMongoIDRegExp = new RegExp('^[0-9a-fA-F]{24}$')
+
+  if (!checkMongoIDRegExp.test(id)) {
+    //  return res.status(400).json({ message: 'El id no es valido' + id })
+    return new Response(JSON.stringify({ message: 'El id no es valido' }), {
+      status: 400,
+      headers: {
+        'Contend-Type': 'aplication/json',
+      },
+    })
+  }
+
+  console.log('MIDDLEWAREEEEEEEEE')
+  return NextResponse.next()
+}
